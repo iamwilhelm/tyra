40a6ebfa85034b4e92d0aea9dadfc5fca412a7a0
ian <ian@ian1.(none)>
2009-11-25 18:47:35 -0500

    use first col instead of total

--- tyra.py
+++ tyra.py
@@ -35,7 +35,7 @@ class Tyra:
         >>> print tyra.lookup('pygmy')
         ['whales|pygmy_whale']
         >>> print tyra.lookup('Banks')
-        ['banks']
+        ['number_of_banks']
         >>> print tyra.lookup('hump heat')
         ['oil|consumption`heat', 'whales|humpback_whale']
         """
@@ -51,22 +51,22 @@ class Tyra:
     def getData(self, dimension, xAxis=None, xAxisLabels=None, zAxis=None):
         """
         >>> tyra = Tyra(2)
-        >>> print tyra.getData('Banks')
-        {'source': ['The Fed'], 'xAxis': 'Year', 'units': 'Buildings', 'xAxisLabels': ['1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992'], 'data': ['5154.87231', '5149.45502', '5146.63868', '5152.8723', '5153.36446', '5153.18858', '5157.43476', '5152.97737', '5154.32464', '5149.66505', '5156.04165', '5152.08343', '5154.48629'], 'dimension': 'Banks'}
+        >>> print tyra.getData('Number_of_Banks')
+        {'source': ['fake'], 'xAxis': 'Year', 'units': 'Buildings', 'xAxisLabels': ['1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992'], 'data': ['5154.87231', '5149.45502', '5146.63868', '5152.8723', '5153.36446', '5153.18858', '5157.43476', '5152.97737', '5154.32464', '5149.66505', '5156.04165', '5152.08343', '5154.48629'], 'dimension': 'Banks'}
         >>> print tyra.getData('Oil|Production')
-        {'source': ['Oil Companies'], 'xAxis': 'Year', 'units': 'Barrels', 'xAxisLabels': ['1995', '1996', '1997', '1998', '1999', '2000'], 'data': ['2592.86774', '2562.55721', '2536.98437', '2506.7974', '2478.74694', '2450.37708'], 'dimension': 'Oil|Production'}
+        {'source': ['fake'], 'xAxis': 'Year', 'units': 'Barrels', 'xAxisLabels': ['1995', '1996', '1997', '1998', '1999', '2000'], 'data': ['2592.86774', '2562.55721', '2536.98437', '2506.7974', '2478.74694', '2450.37708'], 'dimension': 'Oil|Production'}
         >>> print tyra.getData('Oil|Production', 'State', ['Alabama', 'Arizona', 'Alaska'])
-        {'source': ['Oil Companies'], 'xAxis': 'State', 'units': 'Barrels', 'xAxisLabels': ['Alabama', 'Arizona', 'Alaska'], 'data': ['None', 'None', 'None'], 'dimension': 'Oil|Production'}
-        >>> print tyra.getData('Banks', 'State')
-        {'source': ['The Fed'], 'xAxis': 'State', 'units': 'Buildings', 'xAxisLabels': ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'], 'data': ['1312.5775', '1313.57007', '1316.9508', '1314.85217', '1315.20706', '1313.64127', '1313.48346', '1313.98884', '1315.87818', '1311.5455', '1317.07361', '1313.70655', '1314.19676', '1313.28564', '1313.22888', '1309.44748', '1312.55034', '1310.63597', '1311.72927', '1315.75405', '1317.16719', '1311.78164', '1308.10401', '1314.52436', '1310.9932', '1312.62503', '1314.58399', '1315.17477', '1312.8734', '1312.0062', '1314.72654', '1312.02684', '1312.59719', '1312.2725', '1311.51999', '1312.08487', '1316.36195', '1310.31879', '1311.88335', '1311.54587', '1313.23416', '1313.60554', '1318.22429', '1312.21072', '1315.31409', '1319.51368', '1312.94672', '1315.51638', '1313.22002', '1312.21882', '1312.92507'], 'dimension': 'Banks'}
+        {'source': ['fake'], 'xAxis': 'State', 'units': 'Barrels', 'xAxisLabels': ['Alabama', 'Arizona', 'Alaska'], 'data': ['None', 'None', 'None'], 'dimension': 'Oil|Production'}
+        >>> print tyra.getData('Number_of_Banks', 'State')
+        {'source': ['fake'], 'xAxis': 'State', 'units': 'Buildings', 'xAxisLabels': ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'], 'data': ['1312.5775', '1313.57007', '1316.9508', '1314.85217', '1315.20706', '1313.64127', '1313.48346', '1313.98884', '1315.87818', '1311.5455', '1317.07361', '1313.70655', '1314.19676', '1313.28564', '1313.22888', '1309.44748', '1312.55034', '1310.63597', '1311.72927', '1315.75405', '1317.16719', '1311.78164', '1308.10401', '1314.52436', '1310.9932', '1312.62503', '1314.58399', '1315.17477', '1312.8734', '1312.0062', '1314.72654', '1312.02684', '1312.59719', '1312.2725', '1311.51999', '1312.08487', '1316.36195', '1310.31879', '1311.88335', '1311.54587', '1313.23416', '1313.60554', '1318.22429', '1312.21072', '1315.31409', '1319.51368', '1312.94672', '1315.51638', '1313.22002', '1312.21882', '1312.92507'], 'dimension': 'Banks'}
         >>> print tyra.getData('Whales')
-        {'source': ['Greenpeace'], 'xAxis': 'State', 'units': 'Animals', 'xAxisLabels': ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'United States', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'], 'data': ['1015.3908', '1011.80846', '1016.88395', '1005.0795', '1014.40471', '1011.26497', '1010.10672', '1013.04547', '1007.34524', '1017.77531', '1014.25572', '1012.64764', '1013.66977', '1013.80906', '1012.08288', '1015.45676', '1013.18664', '1014.49127', '1012.02625', '1013.58057', '1014.14379', '1014.94953', '1014.24989', '1012.51054', '1010.63554', '1019.6721', '1007.79012', '1014.78186', '1013.23279', '1014.41468', '1017.60857', '1011.838', '1013.04106', '1012.30774', '1013.33232', '1014.87032', '1012.89912', '1015.5886', '1014.51557', '1009.23906', '1014.81607', '1012.92928', '1012.60905', '1013.57413', '51663.7237', '1008.95434', '1012.11217', '1012.26675', '1014.6597', '1010.32854', '1008.18651', '1013.35426'], 'dimension': 'Whales'}
+        {'source': ['fake'], 'xAxis': 'State', 'units': 'Animals', 'xAxisLabels': ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'United States', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'], 'data': ['1015.3908', '1011.80846', '1016.88395', '1005.0795', '1014.40471', '1011.26497', '1010.10672', '1013.04547', '1007.34524', '1017.77531', '1014.25572', '1012.64764', '1013.66977', '1013.80906', '1012.08288', '1015.45676', '1013.18664', '1014.49127', '1012.02625', '1013.58057', '1014.14379', '1014.94953', '1014.24989', '1012.51054', '1010.63554', '1019.6721', '1007.79012', '1014.78186', '1013.23279', '1014.41468', '1017.60857', '1011.838', '1013.04106', '1012.30774', '1013.33232', '1014.87032', '1012.89912', '1015.5886', '1014.51557', '1009.23906', '1014.81607', '1012.92928', '1012.60905', '1013.57413', '51663.7237', '1008.95434', '1012.11217', '1012.26675', '1014.6597', '1010.32854', '1008.18651', '1013.35426'], 'dimension': 'Whales'}
         >>> print tyra.getData('Whales|Humpback_Whale')
-        {'source': ['Greenpeace'], 'xAxis': 'State', 'units': 'Animals', 'xAxisLabels': ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'United States', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'], 'data': ['204.494633', '201.366761', '202.077637', '200.489285', '201.796524', '202.068229', '201.657764', '204.129421', '200.693428', '204.015995', '203.907874', '200.247465', '202.57808', '203.168911', '204.624186', '201.516911', '204.377048', '201.006215', '203.174387', '203.266518', '202.9763', '201.777514', '202.695286', '204.845262', '202.082347', '204.739034', '201.525573', '203.814517', '202.260975', '203.949468', '201.424092', '200.420245', '201.234057', '202.732241', '203.527201', '204.181466', '204.075963', '203.591515', '203.948775', '200.890795', '202.147049', '201.805557', '200.541373', '204.399591', '10329.9659', '201.101616', '203.469599', '203.412227', '201.090062', '201.231441', '200.478065', '202.939433'], 'dimension': 'Whales|Humpback_Whale'}
-        >>> print tyra.getData('Banks', 'Year', ['1980', '1981', '1982'])
-        {'source': ['The Fed'], 'xAxis': 'Year', 'units': 'Buildings', 'xAxisLabels': ['1980', '1981', '1982'], 'data': ['5154.87231', '5149.45502', '5146.63868'], 'dimension': 'Banks'}
+        {'source': ['fake'], 'xAxis': 'State', 'units': 'Animals', 'xAxisLabels': ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'United States', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'], 'data': ['204.494633', '201.366761', '202.077637', '200.489285', '201.796524', '202.068229', '201.657764', '204.129421', '200.693428', '204.015995', '203.907874', '200.247465', '202.57808', '203.168911', '204.624186', '201.516911', '204.377048', '201.006215', '203.174387', '203.266518', '202.9763', '201.777514', '202.695286', '204.845262', '202.082347', '204.739034', '201.525573', '203.814517', '202.260975', '203.949468', '201.424092', '200.420245', '201.234057', '202.732241', '203.527201', '204.181466', '204.075963', '203.591515', '203.948775', '200.890795', '202.147049', '201.805557', '200.541373', '204.399591', '10329.9659', '201.101616', '203.469599', '203.412227', '201.090062', '201.231441', '200.478065', '202.939433'], 'dimension': 'Whales|Humpback_Whale'}
+        >>> print tyra.getData('Number_of_Banks', 'Year', ['1980', '1981', '1982'])
+        {'source': ['fake'], 'xAxis': 'Year', 'units': 'Buildings', 'xAxisLabels': ['1980', '1981', '1982'], 'data': ['5154.87231', '5149.45502', '5146.63868'], 'dimension': 'Banks'}
         >>> print tyra.getData('Peanut_Butter|PBJ', None, ['Alabama', 'Arizona', 'Alaska'])
-        {'source': ['Jiffy'], 'xAxis': 'State', 'units': 'Tons', 'xAxisLabels': ['Alabama', 'Arizona', 'Alaska'], 'data': ['1015.3908', '1016.88395', '1011.80846'], 'dimension': 'Peanut_Butter|PBJ'}
+        {'source': ['fake'], 'xAxis': 'State', 'units': 'Tons', 'xAxisLabels': ['Alabama', 'Arizona', 'Alaska'], 'data': ['1015.3908', '1016.88395', '1011.80846'], 'dimension': 'Peanut_Butter|PBJ'}
         """
         # set category if one is specified
         keyList = dict()
@@ -91,8 +91,7 @@ class Tyra:
             xAxisLabels.sort()
 
         # get list of sorted dimensions for this dataset
-        dims = map(str, meta['dims'].keys())
-        dims.sort();
+        dims = sorted(map(str, meta['dims'].keys()))
 
         # build otherdim key
         otherDims = {}
@@ -103,13 +102,11 @@ class Tyra:
                 else:
                     otherDims[kk] = 'Total'
 
-        # find sum on other dims
+        # TODO find sum on all dims other than xaxis
+        # for now, just use the first col
         for kk in dims:
-            if kk==xAxis:
-                continue
-            if kk=='Category' and not caty == None:
-                continue
-            keyList[kk] = 'Total'
+            if kk!=xAxis and (kk!='Category' or caty==None):
+                keyList[kk] = str(sorted(meta['dims'][kk])[0]) #'Total'
 
         # pull the actual data
         ret = dict()
